fadfbe623ec0754797a91f2eafb32383a8fd97cd
Li <li@silica.codes>
2025-03-26 16:29:07 +1300

    Upload code

---
+++ cdrom/index.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+<title>TamaTown Rewritten - CD Rom</title>
+<meta name="title" content="TamaTown Rewritten">
+<meta name="description" content="Tamagotchi Town Rewritten - Play Tama&Earth EXPO & Dream Town & E-Tamago online for FREE">
+<meta name="keywords" content="TamaTown,E-Tamago,Uritama,Tamagotchi Town,Tamagotchi Town Rewritten,TamaTown Rewritten,Free,Games,Private Server,TamaTown,Familetchi,V5,Tamagotchi Familetchi,Famitama,Celebrity,V5.5,Royal Famitama,Tama&Earth EXPO,Tama,And,Earth,EXPO,Tama&Earth,Tamagotchi Familetchi,Logout Password,Logout No,Login Password,Login No,Tamagotchi V5.5,Tamagotchi V5,Dream Town,DreamTown,Tamagotchi Friends,Tamagotchi Friends Dream Town,Game,Rebuilt,Rebuilding,Waking Up Tamagotchi Town">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="English">
+<meta name="revisit-after" content="1 days">
+<meta name="author" content="Bandai, WIZZ">
+<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+<style>
+	body {
+		background-color: black;
+		color: white;
+	}
+	.center {
+		position: absolute;
+		color: white;
+		left: 20%;
+		top: 40%;
+	}
+	li {
+		list-style-type: none;
+		display: inline-block;
+		text-align: center;
+	}
+	
+	a:link, a:visited, a{
+	  color: white;
+	  font-weight: bold;
+	  text-decoration: none;
+	  cursor: pointer;
+	}
+
+	a:hover{
+	  color: white;
+	  font-weight: bold;
+	  text-decoration: underline;
+	  cursor: pointer;
+	}
+</style>
+</head>
+<body>
+<h1>Tamagotchi DVD/CD Archives</h1>
+<p>Playable-ish in Browser: </p>
+<ul>
+<li><a href="v4.5/SWFs">v4.5 PC Pack</a></li> |
+<li><a href="v5.5">v5.5 Bonus Disc</a></li> |
+<li><a href="entama">Welcome to the world of Entama</a></li>
+</ul>
+<p>Tamagotchi TV: </p>
+<ul>
+<li><a href="https://archive.org/download/familetchi-dvd/TAMAGOTCHI_EURO.ISO"><img src="tamagotchi_tv_euro.jpg" alt="Tamagotchi TV (EURO)" width="300" height="300"></a> <p>(Europe Version)</p></li> 
+<li><a href="https://archive.org/download/familetchi-dvd-jpn/TAMA_DVD.ISO"><img src="tamagotchi_tv_jp.jpg" alt="Tamagotchi TV (JP)" width="300" height="300"></a> <p>(Japanese Version)</p></li> 
+<li><a href="https://archive.org/download/tamagotchi-tv-royal/Tamagotch_Royal_DVD.iso"><img src="tamagotchi_tv_gorgeous_jp.jpg" alt="Tamagotchi TV: Gorgeous" width="300" height="300"></a> <p>(Japanese Royal Version)</p></li>
+</ul>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+<title>TamaTown Rewritten</title>
+<meta name="title" content="TamaTown Rewritten">
+<meta name="description" content="Tamagotchi Town Rewritten - Play Tama&Earth EXPO & Dream Town & E-Tamago online for FREE">
+<meta name="keywords" content="TamaTown,E-Tamago,Uritama,Tamagotchi Town,Tamagotchi Town Rewritten,TamaTown Rewritten,Free,Games,Private Server,TamaTown,Familetchi,V5,Tamagotchi Familetchi,Famitama,Celebrity,V5.5,Royal Famitama,Tama&Earth EXPO,Tama,And,Earth,EXPO,Tama&Earth,Tamagotchi Familetchi,Logout Password,Logout No,Login Password,Login No,Tamagotchi V5.5,Tamagotchi V5,Dream Town,DreamTown,Tamagotchi Friends,Tamagotchi Friends Dream Town,Game,Rebuilt,Rebuilding,Waking Up Tamagotchi Town">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="English">
+<meta name="revisit-after" content="1 days">
+<meta name="author" content="Bandai, WIZZ">
+<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+<style>
+	body {
+		background-color: black;
+	}
+	.center {
+		position: absolute;
+		color: white;
+		left: 20%;
+		top: 40%;
+	}
+	.text {
+		color:white;
+		text-align:center;
+	}
+</style>
+</head>
+<body>
+<div class="text">
+<!--	<b>Now on famitama.com, Thx bandai!</b> -->
+</div>
+<div class="center">
+	<?php
+	if($_SERVER['HTTP_HOST'] == "famita.ml")
+	{
+		echo("<h1>We have moved...</h1>");
+		echo("<h1>Redirecting to famitama.xyz...</h1>");
+		echo("<script>");
+		echo("f = function(){ window.location = 'http://famitama.xyz'; };");
+		echo("setTimeout(f, 3000);");
+		echo("</script>");
+		die();
+	}
+	?>
+
+	
+	<a href="/pc/index.html"><img src="version5.png" width="287" height="87"></a>
+	<a href="/friends/index.html"><img src="friends.png" width="260" height="87"></a>
+	<a href="/etg/pc/index.html"><img src="etamago.png" width="260" height="87"></a>
+	<a href="/cdrom/"><img src="cdrom.png" width="260" height="87"></a></a>
+	
+	<!--<a href="/xp/index.html"><img src="xp.png" width="287" height="87"></a>-->
+	<br><br>
+	<b><a href="/flash.html">Running flash in 2021 and BEYOND!</a></b>
+	<b><a href="/credits.html">View Credits</a></b>
+
+</div>
+</body>
+</html>
+
